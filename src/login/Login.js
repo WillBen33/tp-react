9b18544b7bbd411d540716b06eac0551369de320
wilfrid benoit <wilfrid.benoit@yahoo.fr>
2021-01-05 14:45:13 +0100

    Add login form

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,21 +1,41 @@
 import './Login.css';
 
+import React, { useState } from 'react';
+
+
 export default function Login() {
+
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const seConnecter = (event) => {
+        if (username === 'test' && password === '1234') {
+
+            document.location = '/home';
+        }
+    }
+
+    const handlePassword = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const handleUsername = (event) => {
+        setUsername(event.target.value);
+    }
+
     return(
         <div>
-            <form>
-                <label>
-                    Nom : 
-                    <input type="text" name="name" />
-                </label>
-                <br/>
-                <label>
-                    Mot de passe : 
-                    <input type="password" name="password" />
-                </label>
-                <br/>
-                <input type="submit" value="Envoyer" />
-            </form>
+            <label>
+                Nom : 
+                <input type="text" id="username" value={username} onChange={handleUsername} />
+            </label>
+            <br/>
+            <label>
+                Mot de passe : 
+                <input type="text" id="username" value={password} onChange={handlePassword} />
+            </label>
+            <br/>
+            <input type="submit" value="Envoyer" onClick={seConnecter}/>
         </div>
     )
 }
